fd2896243026ce2dd66d842dadd622efc41969a6
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-21 00:44:45 +0600

    Refactor slider section layout

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -1,75 +1,51 @@
-  <!-- slider section -->
-    <section class="slider_section">
+<!-- Slider Section -->
+<section class="slider_section">
+    <div class="slider_container">
+        <div id="homeSlider" class="carousel slide" data-ride="carousel">
+            
+            <div class="carousel-inner">
+                
+                <!-- Slider Item -->
+                <div class="carousel-item active">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            
+                            <!-- Text Content -->
+                            <div class="col-md-7">
+                                <div class="detail-box">
+                                    <h1>
+                                        Welcome To Our <br>
+                                        Gift Shop
+                                    </h1>
+                                    <p>
+                                        Sequi perspiciatis nulla reiciendis rem, tenetur impedit.
+                                        Eveniet non necessitatibus error distinctio mollitia suscipit.
+                                    </p>
+                                    <a href="{{ url('/contact') }}" class="btn btn-primary">
+                                        Contact Us
+                                    </a>
+                                </div>
+                            </div>
 
-      <div class="slider_container">
+                            <!-- Image -->
+                            <div class="col-md-5">
+                                <div class="img-box">
+                                    <img 
+                                        src="{{ asset('images/image3.jpeg') }}" 
+                                        class="img-fluid slider-img" 
+                                        alt="Gift Shop Slider Image"
+                                    >
+                                </div>
+                            </div>
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-
-          <div class="carousel-inner">
-
-            <div class="carousel-item active">
-
-              <div class="container-fluid">
-
-                <div class="row">
-
-                  <div class="col-md-7">
-
-                    <div class="detail-box">
-
-                      <h1>
-
-                        Welcome To Our <br>
-
-                        Gift Shop
-
-                      </h1>
-
-                      <p>
-
-                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
-
-                      </p>
-
-                      <a href="">
-
-                        Contact Us
-
-                      </a>
-
+                        </div>
                     </div>
-
-                  </div>
-
-                  <div class="col-md-5 ">
-
-                    <div class="img-box">
-
-                      <img style="width:600px" src="images/image3.jpeg" alt="" />
-
-                    </div>
-
-                  </div>
-
                 </div>
-
-              </div>
+                <!-- End Slider Item -->
 
             </div>
 
-           
-
-          </div>
-
-         
-
         </div>
-
-      </div>
-
-    </section>
-
-
-
-    
- <!-- End slider section -->
+    </div>
+</section>
+<!-- End Slider Section -->
