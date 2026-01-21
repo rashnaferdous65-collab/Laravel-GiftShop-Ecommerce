11b05304fd97349c1d73daabedbb5eea284af1ba
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-22 00:09:19 +0600

    Refactor shop blade UI (logic unchanged)

--- resources/views/home/shop.blade.php
+++ resources/views/home/shop.blade.php
@@ -1,50 +1,61 @@
 <!-- shop section -->
-
 <section class="shop_section layout_padding">
     <div class="container">
+
         <div class="heading_container heading_center">
-            <h2 style="font-size:50px; font-weight:bold;">Latest Products</h2>
+            <h2 class="fw-bold" style="font-size:50px;">Latest Products</h2>
         </div>
 
         <div class="row">
 
-            @if(!empty($product) && $product->count() > 0)
-                @foreach($product as $item)
-                    @if(is_object($item)) 
-                        <div class="col-sm-6 col-md-4 col-lg-3">
-                            <div class="box">
+            @forelse($product ?? [] as $item)
 
-                                <div class="img-box">
-                                    @if(!empty($item->image) && file_exists(public_path('products/' . $item->image)))
-                                        <img src="{{ asset('products/' . $item->image) }}" alt="{{ $item->title }}">
-                                    @else
-                                        <img src="{{ asset('images/default.png') }}" alt="Product Image Not Found">
-                                    @endif
-                                </div>
+                @continue(!is_object($item))
 
-                                <div class="detail-box">
-                                    <a href="{{ url('product_details', $item->id) }}">
-                                        {{ $item->title }}
-                                    </a>
-                                    <span>৳ {{ $item->price }}</span>
-                                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="box h-100">
 
-                                <div style=" display: flex;  gap: 10px;">
-                                    <a href="{{ url('product_details', $item->id) }}" class="btn btn-secondary text-white mt-1" style="flex-grow: 1;" >
-                                        View Details
-                                    </a>
-                                <a href="{{ url('add_cart', $item->id) }}" class="btn btn-primary text-white" >Add to Cart</a>
-                                </div>
+                        {{-- Image --}}
+                        <div class="img-box">
+                            @php
+                                $imagePath = !empty($item->image) && file_exists(public_path('products/'.$item->image))
+                                    ? asset('products/'.$item->image)
+                                    : asset('images/default.png');
+                            @endphp
 
-                            </div>
+                            <img src="{{ $imagePath }}" alt="{{ $item->title }}">
                         </div>
-                    @endif
-                @endforeach
-            @else
-                <p class="text-center">No products found.</p>
-            @endif
+
+                        {{-- Details --}}
+                        <div class="detail-box">
+                            <a href="{{ url('product_details', $item->id) }}">
+                                {{ $item->title }}
+                            </a>
+                            <span>৳ {{ $item->price }}</span>
+                        </div>
+
+                        {{-- Buttons --}}
+                        <div class="d-flex gap-2 mt-2">
+                            <a href="{{ url('product_details', $item->id) }}"
+                               class="btn btn-secondary text-white flex-fill">
+                                View Details
+                            </a>
+
+                            <a href="{{ url('add_cart', $item->id) }}"
+                               class="btn btn-primary text-white">
+                                Add to Cart
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+            @empty
+                <p class="text-center mt-4">No products found.</p>
+            @endforelse
 
         </div>
     </div>
 </section>
+
 
