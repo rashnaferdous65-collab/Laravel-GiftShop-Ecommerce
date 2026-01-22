22e4895e79e144107abaa0e58dd1bc512dbd0f95
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-23 00:14:44 +0600

    Refactor add_contact blade layout (no logic change)

--- resources/views/home/add_contact.blade.php
+++ resources/views/home/add_contact.blade.php
@@ -1,64 +1,70 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
+@section('title')
+    Giftshop-That is what makes you surprised.
+@endsection
 
-@section('title', 'Giftshop-That is what makes you surprised.') 
+@section('content')
 
+<div class="hero_area">
+    @include('home.header')
+</div>
 
-@section('content') 
-    
-    <body> 
-
-    <div class="hero_area">
-       
-           
-        @include('home.header') 
-        </div>
-        <!-- contact section -->
-
-  <section class="contact_section layout_padding">
+<!-- Contact Section -->
+<section class="contact_section layout_padding">
     <div class="container px-0">
-      <div class="heading_container ">
-        <h2 class="">
-          Contact Us
-        </h2>
-      </div>
+        <div class="heading_container">
+            <h2>Contact Us</h2>
+        </div>
     </div>
+
     <div class="container container-bg">
-      <div class="row">
-        <div class="col-lg-7 col-md-6 px-0">
-          <div class="map_container">
-            <div class="map-responsive">
-              <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
+        <div class="row">
+
+            <!-- Map Area -->
+            <div class="col-lg-7 col-md-6 px-0">
+                <div class="map_container">
+                    <div class="map-responsive">
+                        <iframe
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
+                            style="border:0; width:100%; height:100%;"
+                            allowfullscreen
+                            loading="lazy">
+                        </iframe>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <!-- Contact Form -->
+            <div class="col-lg-5 col-md-6 px-0">
+                <form action="#">
+                    <div class="mb-3">
+                        <input type="text" placeholder="Name">
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="email" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="text" placeholder="Phone">
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="text" class="message-box" placeholder="Message">
+                    </div>
+
+                    <div class="d-flex">
+                        <button type="submit">SEND</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
-        <div class="col-md-6 col-lg-5 px-0">
-          <form action="#">
-            <div>
-              <input type="text" placeholder="Name" />
-            </div>
-            <div>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div>
-              <input type="text" placeholder="Phone" />
-            </div>
-            <div>
-              <input type="text" class="message-box" placeholder="Message" />
-            </div>
-            <div class="d-flex ">
-              <button>
-                SEND
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
+<!-- End Contact Section -->
 
-  <!-- end contact section -->
+@include('home.info')
 
-   
-    @include('home.info')
-    @endsection
+@endsection
