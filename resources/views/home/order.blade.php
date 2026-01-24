633e1a774b4969971f8771354b2a471cb8efc1e7
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-25 00:30:55 +0600

    Refactor order.blade.php without changing logic

--- resources/views/home/order.blade.php
+++ resources/views/home/order.blade.php
@@ -1,94 +1,73 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
+@section('title', 'Giftshop - That is what makes you surprised.')
 
-@section('title', 'Giftshop-That is what makes you surprised.') 
+@section('content')
 
+<style>
+    .order_div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 60px 0;
+    }
 
-@section('content') 
-    
-    <body> 
+    table{
+        width: 800px;
+        border: 2px solid black;
+        border-collapse: collapse;
+    }
 
-    <style>
+    th{
+        background-color: skyblue;
+        padding: 15px;
+        font-size: 20px;
+        font-weight: bold;
+        color: white;
+    }
 
-        .order_div{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 60px;
-    margin-bottom: 60px;
-}
+    td{
+        padding: 10px;
+        color: black;
+        border: 1px solid skyblue;
+        text-align: center;
+    }
+</style>
 
+<div class="hero_area">
+    @include('home.header')
+</div>
 
-table{
-    border: 2px solid black;
-    width: 800px;
-}
-
-th{
-    background-color: skyblue;
-    padding: 15px;
-    font-size: 20px;
-    font-weight: bold;
-    color: white;
-}
-
-td{
-    color: black;
-    padding: 10px;
-    border: 1px solid skyblue;
-}
-
-
-
-
-    </style>
-
-    <div class="hero_area">
-       
-           
-        @include('home.header') 
-
-        </div>
-
-        <div class="order_div">
-
-        
-        <table>
-
-        
+<div class="order_div">
+    <table>
+        <thead>
             <tr>
                 <th>Product Title</th>
                 <th>Price</th>
-                 <th>Delivary Status</th>
+                <th>Delivery Status</th>
                 <th>Image</th>
-               
             </tr>
-        @foreach($order as $order)
-            <tr>
-                <td>{{$order->product->title}}</td>
-                <td>{{$order->product->price}}</td>
-                                  <td>
-    @if ($order->status == 'Deliverd')
-        Delivered 
-    @else
-        {{ $order->status }}
-    @endif
-</td>
-              
+        </thead>
 
-                <td>
-                   <img src="products/{{$order->product->image}}"  width="80" alt="">
-                </td>
+        <tbody>
+            @foreach($order as $item)
+                <tr>
+                    <td>{{ $item->product->title }}</td>
+                    <td>{{ $item->product->price }}</td>
 
-              
-            </tr>
-          @endforeach
-         
+                    <td>
+                        {{ $item->status == 'Deliverd' ? 'Delivered' : $item->status }}
+                    </td>
 
-        </table>
-    </div>
-        
-      
+                    <td>
+                        <img src="products/{{ $item->product->image }}" width="80" alt="Product Image">
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
-    @include('home.info')
-    @endsection
+@include('home.info')
+
+@endsection
