4a18f4b74b28e218f5929915cd6185f7980c008a
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-26 00:41:04 +0600

    Refactor my_cart blade view (clean Blade syntax)

--- resources/views/home/my_cart.blade.php
+++ resources/views/home/my_cart.blade.php
@@ -1,159 +1,104 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
+@section('title', 'Giftshop - That is what makes you surprised.')
 
-@section('title', 'Giftshop-That is what makes you surprised.') 
+@section('content')
 
-
-@section('content') 
-      <style>
+<style>
 .div_deg{
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
-    margin-top: 60px;
-    margin-bottom: 60px;
+    display:flex;
+    justify-content:space-between;
+    align-items:flex-start;
+    margin:60px 0;
 }
-
 table{
-    border: 2px solid black;
-    width: 800px;
+    width:800px;
+    border:2px solid black;
 }
-
 th{
-    background-color: skyblue;
-    padding: 15px;
-    font-size: 20px;
-    font-weight: bold;
-    color: white;
+    background:skyblue;
+    color:white;
+    padding:15px;
+    font-size:20px;
 }
-
 td{
-    color: black;
-    padding: 10px;
-    border: 1px solid skyblue;
+    padding:10px;
+    border:1px solid skyblue;
 }
-
-.form_container{
-    flex: 1;
-    padding: 20px;
-    margin-right: 20px;
-}
-
-.table_container{
-    flex: 2;
-    padding: 20px;
-}
-
-.form_container div{
-    margin-bottom: 15px;
-}
-
-.form_container label{
-    display: block;
-    margin-bottom: 5px;
-    font-weight: bold;
-}
-
-.form_container input[type="text"],
-.form_container input[type="address"],
-.form_container input[type="number"]{
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-
+.form_container{flex:1;padding:20px;margin-right:20px;}
+.table_container{flex:2;padding:20px;}
 .price_div{
-
+    text-align:center;
+    font-size:30px;
     font-weight:bold;
-    font-size: 30px;
-    margin-top: 30px;
-    margin-bottom: 30px;
-    text-align: center ;
-    padding-left: 30px;
-
+    margin:30px 0;
 }
 </style>
 
-    
-    <body> 
+<div class="hero_area">
+    @include('home.header')
 
-    <div class="hero_area">
-       
-           
-        @include('home.header') 
+    <div class="div_deg">
 
-     
-         <div class="div_deg">
+        {{-- Order Form --}}
+        <div class="form_container">
+            <form action="{{ url('confirm_order') }}" method="POST">
+                @csrf
 
-    <div class="form_container">
-        <form action="{{url('confirm_order')}}" method="POST">
-          @csrf
-            <div>
                 <label>Receiver Name</label>
-                <input type="text" name="name" value="{{Auth::user()->name}}">
-            </div>
+                <input type="text" name="name" value="{{ Auth::user()->name }}">
 
-            <div>
                 <label>Receiver Address</label>
-                <input type="text" name="address" value="{{Auth::user()->address}}">
-            </div>
+                <input type="text" name="address" value="{{ Auth::user()->address }}">
 
-            <div>
                 <label>Receiver Phone Number</label>
-                <input type="number" name="phone" value="{{Auth::user()->phone}}">
-            </div>
+                <input type="number" name="phone" value="{{ Auth::user()->phone }}">
 
-            <div class="div_gap">
-                <input type="submit" value="Cash On Delivary" class="btn btn-primary text-white">
-                <a href="{{url('stripe')}}" class="btn btn-warning text-white">Payment On Card</a>
-            </div>
-        </form>
+                <br>
+                <input type="submit" value="Cash On Delivery" class="btn btn-primary text-white">
+                <a href="{{ url('stripe') }}" class="btn btn-warning text-white">Payment On Card</a>
+            </form>
+        </div>
+
+        {{-- Cart Table --}}
+        <div class="table_container">
+            @php $total = 0; @endphp
+
+            <table>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+
+                @foreach($cart as $item)
+                    <tr>
+                        <td>{{ $item->product->title }}</td>
+                        <td>${{ $item->product->price }}</td>
+                        <td>
+                            <img src="/products/{{ $item->product->image }}" width="80">
+                        </td>
+                        <td>
+                            <form action="{{ route('delete_cart', $item->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Remove</button>
+                            </form>
+                        </td>
+                    </tr>
+
+                    @php $total += $item->product->price; @endphp
+                @endforeach
+            </table>
+        </div>
     </div>
 
-    <div class="table_container">
-        <table>
-            <tr>
-                <th>Product Title</th>
-                <th>Price</th>
-                <th>Image</th>
-                <th>Remove</th>
-            </tr>
-
-            <?php $value = 0; ?>
-
-            @foreach($cart as $cartItem)
-            <tr>
-                <td>{{ $cartItem->product->title }}</td>
-                <td>{{ $cartItem->product->price }}</td>
-
-                <td>
-                    <img src="/products/{{ $cartItem->product->image }}" width="80" alt="Product Image">
-                </td>
-
-                <td>
-                    <form action="{{ route('delete_cart', $cartItem->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger" >Remove</button>
-                    </form>
-                </td>
-            </tr>
-
-            <?php $value += $cartItem->product->price; ?>
-            @endforeach
-
-        </table>
+    <div class="price_div">
+        Total Value of Cart Is: ${{ $total }}
     </div>
 
-</div>
-
-
-<div class="price_div">
-
-<h3>Total Value of Cart Is: ${{$value}}</h3>
-</div>
-    
- 
     @include('home.info')
-    @endsection
+</div>
+
+@endsection
