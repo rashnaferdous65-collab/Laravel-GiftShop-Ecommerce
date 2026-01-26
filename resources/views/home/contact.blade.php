b6cd215428208c5de143bc4036dacef1d351dfc9
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-27 00:12:04 +0600

    Update contact.blade.php with new layout and design

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,47 +1,52 @@
-  <!-- contact section -->
-
-  <section class="contact_section ">
-    <div class="container px-0">
-      <div class="heading_container ">
-        <h2 class="">
-          Contact Us
-        </h2>
-      </div>
-    </div>
-    <div class="container container-bg">
-      <div class="row">
-        <div class="col-lg-7 col-md-6 px-0">
-          <div class="map_container">
-            <div class="map-responsive">
-              <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
-            </div>
-          </div>
+<!-- contact section -->
+<section class="contact_section py-5">
+    <div class="container">
+        <div class="text-center mb-4">
+            <h2>Get in Touch</h2>
         </div>
-        <div class="col-md-6 col-lg-5 px-0">
-          <form action="#">
-            <div>
-              <input type="text" placeholder="Name" />
+
+        <div class="row g-4 align-items-center">
+            <!-- Map column -->
+            <div class="col-lg-7 col-md-6">
+                <div class="map_wrapper shadow-sm rounded">
+                    <iframe 
+                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" 
+                        style="border:0; width:100%; height:350px;" 
+                        allowfullscreen
+                        loading="lazy">
+                    </iframe>
+                </div>
             </div>
-            <div>
-              <input type="email" placeholder="Email" />
+
+            <!-- Form column -->
+            <div class="col-lg-5 col-md-6">
+                <form action="#" class="p-4 border rounded bg-light">
+                    <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control" placeholder="Your Name" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control" placeholder="Your Email" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Phone</label>
+                        <input type="text" class="form-control" placeholder="Your Phone" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Message</label>
+                        <textarea class="form-control" rows="4" placeholder="Write your message..."></textarea>
+                    </div>
+
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary">Send Message</button>
+                    </div>
+                </form>
             </div>
-            <div>
-              <input type="text" placeholder="Phone" />
-            </div>
-            <div>
-              <input type="text" class="message-box" placeholder="Message" />
-            </div>
-            <div class="d-flex ">
-              <button>
-                SEND
-              </button>
-            </div>
-          </form>
         </div>
-      </div>
     </div>
-  </section>
-
-  <br><br><br>
-
-  <!-- end contact section -->
+</section>
+<!-- end contact section -->
