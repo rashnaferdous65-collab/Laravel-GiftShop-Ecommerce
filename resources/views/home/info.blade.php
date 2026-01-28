77fea42f301ac6451eaa035c9bede220adedf028
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-01-29 00:07:51 +0600

    Refactor info section layout

--- resources/views/home/info.blade.php
+++ resources/views/home/info.blade.php
@@ -1,95 +1,90 @@
-  <!-- info section -->
+<!-- Info Section Start -->
+<section class="info_section layout_padding2-top">
 
-  <section class="info_section  layout_padding2-top">
-    <div class="social_container">
-      <div class="social_box">
-        <a href="">
-          <i class="fa fa-facebook" aria-hidden="true"></i>
-        </a>
-        <a href="">
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-        </a>
-        <a href="">
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-        </a>
-        <a href="">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-        </a>
-      </div>
+  <!-- Social Media -->
+  <div class="social_container">
+    <div class="social_box">
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-instagram"></i></a>
+      <a href="#"><i class="fa fa-youtube"></i></a>
     </div>
-    <div class="info_container ">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-lg-3">
-            <h6>
-              ABOUT US
-            </h6>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
-            </p>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <div class="info_form ">
-              <h5>
-                Newsletter
-              </h5>
-              <form action="#">
-                <input type="email" placeholder="Enter your email">
-                <button>
-                  Subscribe
-                </button>
-              </form>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <h6>
-              NEED HELP
-            </h6>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
-            </p>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <h6>
-              CONTACT US
-            </h6>
-            <div class="info_link-box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span> Gb road 123 london Uk </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>+01 12345678901</span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span> demo@gmail.com</span>
-              </a>
-            </div>
+  </div>
+
+  <!-- Info Content -->
+  <div class="info_container">
+    <div class="container">
+      <div class="row">
+
+        <!-- About Us -->
+        <div class="col-md-6 col-lg-3">
+          <h6>ABOUT US</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <!-- Newsletter -->
+        <div class="col-md-6 col-lg-3">
+          <div class="info_form">
+            <h5>Newsletter</h5>
+            <form action="#">
+              <input type="email" placeholder="Enter your email" required>
+              <button type="submit">Subscribe</button>
+            </form>
           </div>
         </div>
+
+        <!-- Help -->
+        <div class="col-md-6 col-lg-3">
+          <h6>NEED HELP</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed do eiusmod tempor incididunt.
+          </p>
+        </div>
+
+        <!-- Contact -->
+        <div class="col-md-6 col-lg-3">
+          <h6>CONTACT US</h6>
+          <div class="info_link-box">
+            <a href="#">
+              <i class="fa fa-map-marker"></i>
+              <span>Gb road 123, London, UK</span>
+            </a>
+            <a href="tel:+0112345678901">
+              <i class="fa fa-phone"></i>
+              <span>+01 12345678901</span>
+            </a>
+            <a href="mailto:demo@gmail.com">
+              <i class="fa fa-envelope"></i>
+              <span>demo@gmail.com</span>
+            </a>
+          </div>
+        </div>
+
       </div>
     </div>
-    <!-- footer section -->
-    <footer class=" footer_section">
-      <div class="container">
-        <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By
-          <a href="https://html.design/">Rashna Ferdous</a>
-        </p>
-      </div>
-    </footer>
-    <!-- footer section -->
+  </div>
 
-  </section>
+  <!-- Footer -->
+  <footer class="footer_section">
+    <div class="container text-center">
+      <p>
+        &copy; <span id="displayYear"></span> All Rights Reserved By
+        <a href="https://html.design/">Rashna Ferdous</a>
+      </p>
+    </div>
+  </footer>
 
-  <!-- end info section -->
+</section>
+<!-- Info Section End -->
 
-  
-  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
-  </script>
-  <script src="{{asset('js/custom.js')}}"></script> 
+<!-- Scripts -->
+<script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+
 
